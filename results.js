ff5d214a50a0c2fdb7a25f10135236f8d038933f
Thomas Stussi <tstussi06@gmail.com>
2020-07-21 12:22:51 -0700

    working with style

--- results.js
+++ results.js
@@ -31,49 +31,24 @@ function mungeEncountered(encounteredResults) {
     return encountered;
 }
 
-const myChart = new Chart(ctx, {
+Chart.defaults.global.defaultFontColor = 'yellow';
+Chart.defaults.global.defaultFontFamily = 'PokemonSolid';
+// eslint-disable-next-line no-undef, no-unused-vars
+let myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: mungeNames(encounteredResults),
         datasets: [{
             label: '# Caught',
             data: mungeCaught(encounteredResults),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            backgroundColor: 'red',
+            borderColor: 'black',
             borderWidth: 1
         }, {
             label: '# Encountered',
             data: mungeEncountered(encounteredResults),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            backgroundColor: 'blue',
+            borderColor: 'black',
             borderWidth: 1
         }]
     },
